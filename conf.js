18a3905a8ff0794c090bdc60555937bd0386af67
Willard Wong <ifyour@outlook.com>
2019-12-03 12:49:09 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,24 +1,45 @@
 jsproxy_config({
-  // 当前配置的版本（服务端记录在日志中，方便排查问题）
-  ver: '55',
+  // 当前配置的版本（记录在日志中，用于排查问题）
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
+  ver: '118',
+
+  // 通过 CDN 加速常用网站的静态资源（实验中）
+  static_boost: {
+    enable: true,
+    ver: 79
+  },
 
   // 节点配置
   node_map: {
+    'demo-hk': {
+      label: '演示服务-香港节点',
+      lines: {
+        // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
+      }
+    },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
     'mysite': {
       label: '当前站点',
-      lines: [
-        // 静态资源和代理接口位于同个服务器的场合
-        // 例如默认的 ip.xip.io 以及 cloudflare worker
-        location.host
-      ]
+      lines: {
+        [location.host]: 1,
+      }
     },
+    // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: 'Cloudflare Worker',
+      label: '',
       hidden: true,
-      lines: [
-        // 实验中...
-        'node-cfworker.etherdream.com:8443'
-      ]
+      lines: {
+        'node-cfworker-2.etherdream.com': 1,
+      }
     }
   },
 
@@ -26,6 +47,7 @@ jsproxy_config({
    * 默认节点
    */
   node_default: 'mysite',
+  // node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -40,6 +62,12 @@ jsproxy_config({
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
+
+  // 首页路径
+  index_path: 'index_v3.html',
+
+  // 支持 CORS 的站点列表（实验中...）
+  direct_host_list: 'cors_v1.txt',
 
   /**
    * 自定义注入页面的 HTML
